55f46d1ed04e410ac8ba2fbdf63416916451e078
Aninu <30773817+aaninu@users.noreply.github.com>
2018-03-18 23:29:03 +0200

    Create users.sql

---
+++ Databases/users.sql
@@ -0,0 +1,15 @@
+-- ------------------------------------
+-- Table structure for `webph_users`
+-- ------------------------------------
+
+CREATE TABLE `webph_users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `s_email` varchar(100) CHARACTER SET utf8 DEFAULT NULL,
+  `s_nume` varchar(100) CHARACTER SET utf8 DEFAULT NULL,
+  `s_prenume` varchar(100) CHARACTER SET utf8 DEFAULT NULL,
+  `s_telefon` varchar(25) CHARACTER SET utf8 DEFAULT NULL,
+  `s_addresa` varchar(25) CHARACTER SET utf8 DEFAULT NULL,
+  `d_start` varchar(25) CHARACTER SET utf8 DEFAULT NULL,
+  `d_login` varchar(25) CHARACTER SET utf8 DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_german1_ci;
