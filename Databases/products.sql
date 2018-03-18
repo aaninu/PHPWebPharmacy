7ebe5901debcb95185579b776435e7072c5c7bfa
Aninu <30773817+aaninu@users.noreply.github.com>
2018-03-18 23:32:16 +0200

    Create products.sql

---
+++ Databases/products.sql
@@ -0,0 +1,13 @@
+-- ------------------------------------
+-- Table structure for `webph_products`
+-- ------------------------------------
+
+CREATE TABLE `webph_products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `i_user` int(11) NULL,
+  `s_nume` varchar(100) CHARACTER SET utf8 DEFAULT NULL,
+  `d_public` varchar(25) CHARACTER SET utf8 DEFAULT NULL,
+  `d_edit` varchar(25) CHARACTER SET utf8 DEFAULT NULL,
+  ...
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_german1_ci;
