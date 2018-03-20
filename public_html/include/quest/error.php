22a17b80f1bcf440958deb721067cb0dd66a65c4
Aninu <30773817+aaninu@users.noreply.github.com>
2018-03-20 22:27:43 +0200

    Delete error.php

--- public_html/include/quest/error.php
+++
@@ -1,5 +0,0 @@
-Error Page
-<a href="<?=u('login/')?>"> Conectare </a>
-<a href="<?=u('register/')?>"> Inregistrare </a>
-<a href="<?=u('user/')?>"> Zona utilizator </a>
-<a href="<?=u('logout/')?>"> Deconectare </a>
