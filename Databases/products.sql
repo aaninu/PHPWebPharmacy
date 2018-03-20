4a0dacec036d7de466a2772363e3e1b193c9def0
Aninu <30773817+aaninu@users.noreply.github.com>
2018-03-20 11:21:14 +0200

    Update products.sql

--- Databases/products.sql
+++ Databases/products.sql
@@ -8,6 +8,5 @@ CREATE TABLE `webph_products` (
   `s_nume` varchar(100) CHARACTER SET utf8 DEFAULT NULL,
   `d_public` varchar(25) CHARACTER SET utf8 DEFAULT NULL,
   `d_edit` varchar(25) CHARACTER SET utf8 DEFAULT NULL,
-  ...
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_german1_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
