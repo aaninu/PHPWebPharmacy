9ad8418afb85396a2c3c263412e991bbd85ba6af
Aninu <30773817+aaninu@users.noreply.github.com>
2018-03-20 13:15:30 +0200

    Update users.sql

--- Databases/users.sql
+++ Databases/users.sql
@@ -3,13 +3,15 @@
 -- ------------------------------------
 
 CREATE TABLE `webph_users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `s_email` varchar(100) CHARACTER SET utf8 DEFAULT NULL,
-  `s_nume` varchar(100) CHARACTER SET utf8 DEFAULT NULL,
-  `s_prenume` varchar(100) CHARACTER SET utf8 DEFAULT NULL,
-  `s_telefon` varchar(25) CHARACTER SET utf8 DEFAULT NULL,
-  `s_addresa` varchar(25) CHARACTER SET utf8 DEFAULT NULL,
-  `d_start` varchar(25) CHARACTER SET utf8 DEFAULT NULL,
-  `d_login` varchar(25) CHARACTER SET utf8 DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+  `id` int(11) NOT NULL,
+  `s_email` varchar(100) DEFAULT NULL,
+  `s_nume` varchar(100) DEFAULT NULL,
+  `s_prenume` varchar(100) DEFAULT NULL,
+  `sPW_Code` varchar(250) NOT NULL,
+  `sPW_Free` blob NOT NULL,
+  `s_telefon` varchar(25) DEFAULT NULL,
+  `s_addresa` varchar(25) DEFAULT NULL,
+  `d_start` varchar(25) DEFAULT NULL,
+  `d_login` varchar(25) DEFAULT NULL,
+  `eType` enum('ADMIN','MEMBRU','PHARMACY','') NOT NULL DEFAULT 'MEMBRU'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
