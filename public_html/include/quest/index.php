099da99e5da89e6616357483d3d3c99259dd893d
Aninu <30773817+aaninu@users.noreply.github.com>
2018-03-20 22:28:27 +0200

    Delete index.php

--- public_html/include/quest/index.php
+++
@@ -1,6 +0,0 @@
-Index Page
-<a href="<?=u('products/')?>"> Lista cu produse </a>
-<a href="<?=u('login/')?>"> Conectare </a>
-<a href="<?=u('register/')?>"> Inregistrare </a>
-<a href="<?=u('user/')?>"> Zona utilizator </a>
-<a href="<?=u('logout/')?>"> Deconectare </a>
