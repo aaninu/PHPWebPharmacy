0918478af2fec21ba07d74b6b40ca96a29e33f35
Aninu <30773817+aaninu@users.noreply.github.com>
2018-03-22 15:28:10 +0200

    Update products.sql

--- Databases/products.sql
+++ Databases/products.sql
@@ -8,6 +8,7 @@ CREATE TABLE `webph_products` (
   `s_nume` varchar(100) DEFAULT NULL,
   `s_pret` varchar(25) NOT NULL DEFAULT '0',
   `s_moneda` varchar(25) NOT NULL,
+  `s_reducere` varchar(25) NOT NULL DEFAULT '0',
   `i_cantitate` int(11) NOT NULL DEFAULT '1',
   `s_descriere` longtext NOT NULL,
   `s_Tip` longtext NOT NULL,
@@ -15,7 +16,8 @@ CREATE TABLE `webph_products` (
   `s_imagine` longtext NOT NULL,
   `d_expirare` varchar(25) NOT NULL,
   `d_public` varchar(25) DEFAULT NULL,
-  `d_edit` varchar(25) DEFAULT NULL
+  `d_edit` varchar(25) DEFAULT NULL,
+  `i_views` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- Indexes for table `webph_products`
